2eb3ab187ac7075ce4c1ea8df47bd64e6182575a
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-08-23 12:02:00 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,10 @@
+export const globalAudio = new Audio();
+
+export const state = {
+  isUserSeeking: false,
+  currentUser: null,
+  userHistory: {},
+  selectedItem: {},
+  searchTimeout: null,
+  isSignUp: false
+};
