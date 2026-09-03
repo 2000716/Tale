a76d04be685dc11f949ed3143a2a2570bdcf2fc9
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-09-04 01:30:48 +0200

    up

--- ios/App/App/SceneDelegate.swift
+++ ios/App/App/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
 
         window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = UIColor(red: 11.0 / 255.0, green: 17.0 / 255.0, blue: 44.0 / 255.0, alpha: 1.0)
         window?.rootViewController = CAPBridgeViewController()
         window?.makeKeyAndVisible()
 
